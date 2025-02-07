1a68e9f925df65043122e40931d6c175bbb2d1cb
david <dd19900618@gmail.com>
2025-02-07 15:02:22 +0100

    server for showing pod name + k3s deploy file added for deployment+ load balancer

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 
 DOCKER_USERNAME="david8bullet"
+VERSION=$(date +%Y%m%d%H%M%S)  # Example version based on current timestamp
+
 services=(
   "frontend:./Dockerfile.frontend"
   "backend:./Dockerfile.server"
   "backend-python:./Dockerfile.python"
   "upload-service:./Dockerfile.upload"
+  "http-server:./Dockerfile.httpServer"
 )
 
 for service in "${services[@]}"; do
   IFS=":" read -r service_name dockerfile <<< "$service"
 
   echo "Building image for $service_name using $dockerfile..."
-  if ! docker build -t "$DOCKER_USERNAME/$service_name:latest" -f "$dockerfile" .; then
+  if ! docker build -t "$DOCKER_USERNAME/$service_name:$VERSION" -f "$dockerfile" .; then
     echo "Error: Failed to build image for $service_name. Exiting..."
     exit 1
   fi
 
-  echo "Pushing $DOCKER_USERNAME/$service_name:latest to Docker Hub..."
-  if ! docker push "$DOCKER_USERNAME/$service_name:latest"; then
+  echo "Pushing $DOCKER_USERNAME/$service_name:$VERSION to Docker Hub..."
+  if ! docker push "$DOCKER_USERNAME/$service_name:$VERSION"; then
     echo "Error: Failed to push image for $service_name. Exiting..."
     exit 1
   fi
 
-  echo "Successfully built and pushed $service_name!"
+  echo "Successfully built and pushed $service_name:$VERSION!"
   echo "--------------------------------------"
 done
 
-echo "All images have been successfully built and pushed!"
+echo "All images have been successfully built and pushed with version $VERSION!"
